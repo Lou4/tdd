47882dc507c49553dacee041d03e589b6f513885
Lou4 <tagliabue.luca@gmail.com>
2016-04-16 16:18:47 +0200

    VERDE: int array sort

--- UdemyJUnit/src/main/java/com/utils/MyArrayUtils.java
+++ UdemyJUnit/src/main/java/com/utils/MyArrayUtils.java
@@ -3,6 +3,18 @@ package com.utils;
 public class MyArrayUtils {
 	
 	public int[] sort(int[] array){
-		return null;
+		int limit = array.length-1;
+		
+		while(limit >= 1){
+			for(int curr = 0, next = 1, tmp = 0; next<=limit; curr++, next++){
+				if(array[curr] > array[next]){
+					tmp = array[curr];
+					array[curr] = array[next];
+					array[next] = tmp;
+				}
+			}
+			limit--;
+		}
+		return array;
 	}
 }
--- UdemyJUnit/src/test/java/com/utils/MyArrayUtilsTest.java
+++ UdemyJUnit/src/test/java/com/utils/MyArrayUtilsTest.java
@@ -17,5 +17,8 @@ public class MyArrayUtilsTest {
 	@Test
 	public void testArraySort_NotNullValue(){
 		assertArrayEquals(new int[]{1, 2, 3}, myArrayUtils.sort(new int[]{2, 1, 3}));
+		assertArrayEquals(new int[]{1, 2, 3, 4, 5}, myArrayUtils.sort(new int[]{5, 2, 1, 3, 4}));
+		assertArrayEquals(new int[]{1, 2}, myArrayUtils.sort(new int[]{2, 1}));
+		assertArrayEquals(new int[]{1}, myArrayUtils.sort(new int[]{1}));
 	}
 }
