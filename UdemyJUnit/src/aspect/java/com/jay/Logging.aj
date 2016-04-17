cde32140b0fcfd18e2db9cd4e288a0ec30dfbb10
Lou4 <tagliabue.luca@gmail.com>
2016-04-17 12:12:29 +0200

    add a condition to aspect log  pointcut

--- UdemyJUnit/src/aspect/java/com/jay/Logging.aj
+++ UdemyJUnit/src/aspect/java/com/jay/Logging.aj
@@ -1,10 +1,12 @@
 package com.jay;
 
 import com.utils.MyStringUtilsTest;
+import com.utils.MyArrayUtilsTest;
 
 public aspect Logging {
 	
-	pointcut loggingTestExecution(): execution(void MyStringUtilsTest.test*());
+	pointcut loggingTestExecution(): execution(void MyStringUtilsTest.test*())
+					|| execution(void MyArrayUtilsTest.test*());
 	
 	after() : loggingTestExecution(){
 		System.out.println(thisJoinPoint.getSignature().getName() + ":  test eseguito");
--- UdemyJUnit/src/test/java/com/utils/MyArrayUtilsTest.java
+++ UdemyJUnit/src/test/java/com/utils/MyArrayUtilsTest.java
@@ -1,6 +1,8 @@
 package com.utils;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +10,16 @@ import static org.junit.Assert.*;
 public class MyArrayUtilsTest {
 	
 	private MyArrayUtils myArrayUtils;
+	
+	@BeforeClass
+	public static void start(){
+		System.out.println("- - - TESTS BEGIN - - -");
+	}
+	
+	@AfterClass
+	public static void end(){
+		System.out.println("- - - TESTS END - - -");
+	}
 	
 	@Before
 	public void setup(){
@@ -20,5 +32,12 @@ public class MyArrayUtilsTest {
 		assertArrayEquals(new int[]{1, 2, 3, 4, 5}, myArrayUtils.sort(new int[]{5, 2, 1, 3, 4}));
 		assertArrayEquals(new int[]{1, 2}, myArrayUtils.sort(new int[]{2, 1}));
 		assertArrayEquals(new int[]{1}, myArrayUtils.sort(new int[]{1}));
+	}
+	
+	@Test(timeout=100)
+	public void testArraySort_performance(){
+		for(int i = 0; i<1000000; i++){
+			myArrayUtils.sort(new int[]{5, 2, 1, 3, 4});
+		}
 	}
 }
